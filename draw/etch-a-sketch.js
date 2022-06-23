843e12c2a13daf18d87251a98b8894b4eb9e78ad
Johan <jpettersen50@gmail.com>
2022-06-24 01:05:56 +0200

    add colorpicker

--- draw/etch-a-sketch.js
+++ draw/etch-a-sketch.js
@@ -14,8 +14,8 @@ let yAmount = 30;
 function listenGridbox() {
     for (let i = 0; i < gridboxes.length; i++) {
 
-        gridboxes[i].addEventListener("mouseover", () => {
-            gridboxes[i].style.backgroundColor = "gray";
+        gridboxes[i].addEventListener("mousedown", () => {
+            gridboxes[i].style.backgroundColor = "black";
             gridboxes[i].style.width = containerWidth / xAmount;
             gridboxes[i].style.height = containerHeight / yAmount;
         });
@@ -75,3 +75,74 @@ function initiateEtch() {
     createGridBox();
 
 }
+
+
+
+///////////////////////////////////////////<---------Color Picker--------->//////////////////////////////////////////
+
+class ColorPicker {
+    constructor(root) {
+        this.root = root;
+        this.colorjoe = colorjoe.rgb(this.root.querySelector(".color-picker"));
+        this.selectedColor = null;
+        this.savedColors = this.getSavedColors();
+
+
+        this.colorjoe.show();
+        this.setSelectedColor("#009578");
+
+        this.colorjoe.on("change", color => {
+            this.setSelectedColor(color.hex(), true);
+        });
+
+        this.root.querySelectorAll(".saved-color").forEach((el, i) => {
+            this.showSavedColor(el, this.savedColors[i]);
+
+            el.addEventListener("mouseup", e => {
+                if(e.button === 1) {
+                    this.saveColor(this.selectedColor, i);
+                    this.showSavedColor(el, this.selectedColor);
+                }
+
+                this.setSelectedColor(el.dataset.color);
+            });
+        });
+    }
+
+
+    setSelectedColor(color, skipCjUpdate = false) {
+        this.selectedColor = color;
+        this.root.querySelector(".selected-color-text").textContent = color;
+        this.root.querySelector(".selected-color").style.background = color;
+
+        if(!skipCjUpdate) {
+            this.colorjoe.set(color);
+        }
+
+    }
+
+    getSavedColors() {
+        const saved = JSON.parse(localStorage.getItem("colorpicker-saved") || "[]");
+        console.log(saved);
+
+        return Array(6).fill("#ffffff").map((defaultColor, i) => {
+            return saved[i] || defaultColor;
+        })
+    }
+
+    showSavedColor(el, color) {
+        el.style.background = color;
+        el.dataset.color = color;
+    }
+
+    saveColor(color, i) {
+        this.savedColors[i] = color;
+        localStorage.setItem("colorpicker-saved", JSON.stringify(this.savedColors));
+    }
+
+
+}
+
+
+
+const cp = new ColorPicker(document.querySelector(".container"));
