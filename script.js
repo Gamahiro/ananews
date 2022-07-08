5aca238285e710caa9fd5b48b53fbd831c960bb5
Johan <jpettersen50@gmail.com>
2022-07-08 02:34:10 +0200

    ADD login

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const drawButton = document.getElementById("drawButton");
+const loginButton = document.getElementById("loginButton");
+
+loginButton.addEventListener('click', () => {
+    open("./posting-news/posting-news.html");
+});
+
 
 drawButton.addEventListener('click', () => {
     open("./draw/draw.html");
